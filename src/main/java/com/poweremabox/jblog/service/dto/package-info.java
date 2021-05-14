e134b638416566cc1640235d52a6be36d778d255
Michal <emgasuk@gmail.com>
2021-05-14 14:33:29 +0100

    Initial version of JBlog generated by JHipster-7.0.1

---
+++ src/main/java/com/poweremabox/jblog/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.poweremabox.jblog.service.dto;
---
+++ src/test/java/com/poweremabox/jblog/ArchTest.java
@@ -0,0 +1,29 @@
+package com.poweremabox.jblog;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.poweremabox.jblog");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.poweremabox.jblog.service..")
+            .or()
+            .resideInAnyPackage("com.poweremabox.jblog.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.poweremabox.jblog.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
